56f0ddfa4d24371d80a4e56b79b6ab4f6aa16930
jefferson254 <jefferwell2522@gmail.com>
2020-08-24 18:19:31 -0500

    jefferson

---
+++ app/Http/Requests/Institution/CreateCourseRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateCourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'event_type' => ['required', 'min:4', 'max:200'],
+            'institution' => ['required', 'min:4', 'max:200'],
+            'event_name' => ['required', 'min:4', 'max:200'],
+            'start_date' => ['required', 'date_format:d.m.Y' ],
+            'finish_date' => ['required', 'date_format:d.m.Y' ],
+            'hours' => ['required', 'date'],
+            'type_certification' => ['required', 'min:4', 'max:198'],
+        ];
+    }
+}
