83250bb00daba0415a50d1143da82e1c9d7ba24f
LAPTOP-H0DNF93U\hp <slendymolina@gmail.com>
2020-08-25 11:31:24 -0500

    Cambios client Academic Formation

--- dev-client-web-ignug/src/app/models/job-board/models.index.ts
+++ dev-client-web-ignug/src/app/models/job-board/models.index.ts
@@ -2,3 +2,5 @@ export {Professional} from './professional';
 export {Attendance2} from './attendance2';
 export {Course} from './course';
 export {ProfessionalExperience} from './ProfessionalExperience';
+export {AcademicFormation} from './academicFormation';
+export {ProfessionalReferences} from './professionalReferences';
