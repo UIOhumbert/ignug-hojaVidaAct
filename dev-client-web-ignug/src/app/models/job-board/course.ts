7035f5322628214e2cedcc67acc9cc34e0df2180
UIOhumbert <thehumbert@hotmail.com>
2020-08-25 11:17:36 -0500

    angular course

--- dev-client-web-ignug/src/app/models/job-board/course.ts
+++ dev-client-web-ignug/src/app/models/job-board/course.ts
@@ -1,14 +1,20 @@
 import {Catalogue} from '../attendance/models.index';
 import {State} from '../ignug/models.index';
 import {Attendance2} from './models.index';
+import {Professional} from './models.index';
 ////
 export class Course {
     id: number;
     catalogue: Catalogue;
     state: State;
     attendance2: Attendance2;
+    professional: Professional;
+    event_name: string;
+    start_date: Date;
+    finish_date: Date;
+    hours: string;
     constructor() {
-        ///this.date = new Date();
+        //this.date = new Date();
 
     }
 }
